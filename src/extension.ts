edb3400380d468da5ce3d13fb4e974d99fae1804
Garrick Aden-Buie <garrick@adenbuie.com>
2024-05-23 18:52:47 -0400

    feat: Have the old extension remove itself

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,17 @@
 import * as vscode from "vscode";
 
-export async function activate(
-  context: vscode.ExtensionContext
-): Promise<void> {
-  // This shell `Posit.shiny-python` extension depends on the new `Posit.shiny`
-  // extension. In the unlikely event the new extension is not installed, this
-  // check will prompt the user to install it. The new extension handles
-  // uninstallation or deactivation of the `Posit.shiny-python` extension.
-  if (vscode.extensions.getExtension("Posit.shiny")) {
-    return;
+export async function activate(): Promise<void> {
+  // If the Posit.shiny extension is installed, this old shell extension is no
+  // longer needed.
+  if (!vscode.extensions.getExtension("Posit.shiny")) {
+    await vscode.commands.executeCommand(
+      "workbench.extensions.installExtension",
+      "Posit.shiny"
+    );
   }
 
-  const response = await vscode.window.showInformationMessage(
-    "This extension has been deprecated. Please install the new extension 'Shiny' by Posit.",
-    "Install now",
-    "Show extension"
+  vscode.commands.executeCommand(
+    "workbench.extensions.uninstallExtension",
+    "Posit.shiny-python"
   );
-
-  switch (response) {
-    case "Install now":
-      vscode.commands.executeCommand(
-        "workbench.extensions.installExtension",
-        "Posit.shiny"
-      );
-      break;
-
-    case "Show extension":
-      vscode.commands.executeCommand(
-        "workbench.extensions.search",
-        "Posit.shiny"
-      );
-      break;
-
-    default:
-      break;
-  }
 }

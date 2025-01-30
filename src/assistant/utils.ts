37a0b7d20c7544e1d121659c28a49d763a444435
Winston Chang <winston@posit.co>
2025-01-29 22:48:53 -0600

    Add type

--- src/assistant/utils.ts
+++ src/assistant/utils.ts
@@ -1,4 +1,4 @@
-export function inferFileType(filename: string): string {
+export function inferFileType(filename: string): "python" | "r" | "text" {
   const ext = filename.slice(filename.lastIndexOf(".")).toLowerCase();
   switch (ext) {
     case ".py":

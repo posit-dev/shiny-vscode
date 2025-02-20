2919401aee4bafde3654a2814f75f6d421246356
Winston Chang <winston@posit.co>
2025-02-20 13:05:02 -0600

    Change shiny version check tool to general package version check tool

--- src/assistant/tools.ts
+++ src/assistant/tools.ts
@@ -45,14 +45,16 @@ tools.push({
   },
 });
 
-interface LanguageParams {
+interface CheckPackageVersionParams {
   language: "r" | "python";
+  package: string;
+  minVersion?: string;
 }
 
 tools.push({
-  name: "shiny-checkVersionTool",
+  name: "shiny-assistant_checkPackageVersionTool",
   description:
-    "Get the version of Shiny that is installed on the user's system.",
+    "Get the version of an R or Python package that is installed on the user's system.",
   inputSchema: {
     type: "object",
     properties: {
@@ -61,11 +63,21 @@ tools.push({
         enum: ["r", "python"],
         description: "Programming language that Shiny is being used with",
       },
+      package: {
+        type: "string",
+        description: "Name of package to check the version of",
+      },
+      minVersion: {
+        type: "string",
+        description: "Minimum version of the package required",
+      },
     },
+    required: ["language", "package"],
+
     additionalProperties: false,
   },
   invoke: async (
-    options: vscode.LanguageModelToolInvocationOptions<LanguageParams>,
+    options: vscode.LanguageModelToolInvocationOptions<CheckPackageVersionParams>,
     token: vscode.CancellationToken
   ): Promise<vscode.LanguageModelToolResult> => {
     const params = options.input;
@@ -86,10 +98,13 @@ tools.push({
       }
 
       const versionCheckCode = `
-if (system.file(package = "shiny") == "") {
-  cat("not installed")
+cat("Language: ${params.language}\\nPackage: ${params.package}\\n")
+if (system.file(package = "${params.package}") == "") {
+  cat("Version: not installed\\n")
 } else {
-  cat(as.character(packageVersion("shiny")))
+  version <- packageVersion("${params.package}")
+  cat("Version:", as.character(version), "\\n")
+  cat("Is greater or equal to min version (${params.minVersion}):", version >= "${params.minVersion}", "\\n")
 }
 `;
       args = ["-e", versionCheckCode];
@@ -101,12 +116,59 @@ if (system.file(package = "shiny") == "") {
         ]);
       }
 
+      // Python code to check that a version number is greater or equal to
+      // another. It would be nicer to use packaging.version, but that's not
+      // part of the standard library.
+      // Test cases for version_ge():
+      //   assert not version_ge("0.dev16+g83", "0.0.1")
+      //   assert not version_ge("0.3.dev16+g83", "0.3.1")
+      //   assert version_ge("0.3.1.dev16+g83", "0.3.1")
+      //   assert not version_ge("0.3.1", "0.3.1.dev16+g83")
+      //   assert not version_ge("0.3.1.dev16+g83", "0.3.2")
+      //   assert version_ge("0.3.1.dev16+g83", "0.3.1.dev15")
+      //   assert version_ge("0.3.1.dev15+g83", "0.3.1.dev15")
+      //   assert not version_ge("0.3.1.dev15+g83", "0.3.1.dev16")
+      //   assert version_ge("0.3.1dev16", "0.3.1")
+      //   assert version_ge("0.3.1.dev16", "0.3.1")
+      //   assert not version_ge("0.3.dev16+g83", "0.3.1")
+      //   assert not version_ge("0.3.0dev16", "0.3.1")
       const versionCheckCode = `
+def version_ge(version1: str, version2: str):
+  # First drop everything after '+'
+  version1 = version1.split("+")[0]
+  version2 = version2.split("+")[0]
+
+  def split_version(v: str) -> list[str]:
+    # First split on '.dev'
+    v = v.replace(".dev", ".").replace("dev", ".")
+    parts = v.split(".")
+    return parts
+
+  parts1 = [int(x) for x in split_version(version1)]
+  parts2 = [int(x) for x in split_version(version2)]
+
+  max_length = max(len(parts1), len(parts2))
+  parts1 += [0] * (max_length - len(parts1))
+  parts2 += [0] * (max_length - len(parts2))
+
+  for part1, part2 in zip(parts1, parts2):
+    if part1 > part2:
+      return True
+    elif part1 < part2:
+      return False
+
+  return True
+
+print("Language: ${params.language}\\nPackage: ${params.package}")
+
 try:
-  import shiny
+  from importlib.metadata import version
+  import ${params.package}
+  ver = version("${params.package}")
+  print("Version:", ver)
+  print("Is greater or equal to min version (${params.minVersion}):", version_ge(ver, "${params.minVersion}"))
 except ImportError:
-  print('not installed')
-print(shiny.__version__)
+  print("Version: not installed")
 `;
       args = ["-c", versionCheckCode];
     } else {
@@ -123,9 +185,9 @@ print(shiny.__version__)
     });
 
     if (cmdResult.status === "error") {
-      resultString = `Error getting Shiny version.`;
+      resultString = `Error getting version of ${params.package}.`;
     } else {
-      resultString = `Shiny for ${params.language} version: ${cmdResult.stdout.join("")}`;
+      resultString = cmdResult.stdout.join("");
     }
 
     return new vscode.LanguageModelToolResult([
